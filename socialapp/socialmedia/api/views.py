f2a8353977252d15b64ac8317f0be84587ad4112
Blessy <blessypsaji@gmail.com>
2022-08-30 14:45:45 +0530

    this is sm

--- socialapp/socialmedia/api/views.py
+++ socialapp/socialmedia/api/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ViewSet
+from api.serializers import PostSerializer
+from rest_framework.response import Response
+from api.models import Posts
+class PostsView(ViewSet):
+    def list(self,request,*args,**kwargs):
+        qs=Posts.objects.all()
+        serializer=PostSerializer(qs,many=True)
+        return Response(data=serializer.data)
+    def create(self,request,*args,**kwargs):
+        serializer=PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data)
+        else:
+            return Response(data=serializer.errors)
+    def retrieve(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        qs=Posts.objects.get (id=id)
+        serializer=PostSerializer(qs)
+        return Response(data=serializer.data)
+    def update(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        instance=Posts.objects.get(id=id)
+        serializer=PostSerializer(instance=instance,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data)
+        else:
+            return Response(data=serializer.errors)
+    def destroy(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        instance=Posts.objects.get(id=id)
+        instance.delete()
+        return Response ({"msg":"deleted"})
